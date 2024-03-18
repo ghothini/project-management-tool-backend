cd480773e56c7065bbe8a3beff2c1f0b05b8833f
Thapelo Mokotelakoena <thapeloghothini@gmail.com>
2024-03-18 15:43:11 +0200

    Added login route

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,30 +3,31 @@ const router = express.Router();
 const Controlls = require('../Controllers/Controlls')
 
 // Get
-router.get('/',Controlls.defaultEndPoint)
-router.get('/get-users',Controlls.getUsers)
-router.get('/get-projects',Controlls.getProjects)
-router.get('/get-tasks',Controlls.getTasks)
-router.get('/assigned-tasks',Controlls.getAssignedTasks)
+router.get('/', Controlls.defaultEndPoint)
+router.get('/get-users', Controlls.getUsers)
+router.get('/login/:email/:password', Controlls.login)
+router.get('/get-projects', Controlls.getProjects)
+router.get('/get-tasks', Controlls.getTasks)
+router.get('/assigned-tasks', Controlls.getAssignedTasks)
 
 // Post
-router.post('/add-user',Controlls.addUser)
-router.post('/add-project',Controlls.addProject)
-router.post('/add-task',Controlls.addTask)
-router.post('/assign-task',Controlls.assignTask)
+router.post('/add-user', Controlls.addUser)
+router.post('/add-project', Controlls.addProject)
+router.post('/add-task', Controlls.addTask)
+router.post('/assign-task', Controlls.assignTask)
 // router.post('/sendPassword',)
 
 
 // Update
-router.post('/update-user',Controlls.updateUser)
-router.post('/update-project',Controlls.updateProject)
-router.post('/update-member-task',Controlls.updateTask)
+router.post('/update-user', Controlls.updateUser)
+router.post('/update-project', Controlls.updateProject)
+router.post('/update-member-task', Controlls.updateTask)
 
 // Delete
-router.delete('/delete-user/:email',Controlls.deleteUser)
-router.delete('/delete-project/:projectDescription',Controlls.deleteProject)
-router.delete('/delete-task/:taskDescription',Controlls.deleteTask)
-router.delete('/delete-assigned-task/:_id',Controlls.deleteAssignedTask)
+router.delete('/delete-user/:email', Controlls.deleteUser)
+router.delete('/delete-project/:projectDescription', Controlls.deleteProject)
+router.delete('/delete-task/:taskDescription', Controlls.deleteTask)
+router.delete('/delete-assigned-task/:_id', Controlls.deleteAssignedTask)
 
 module.exports = router;
 
